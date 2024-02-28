7c7fd30bf6cbb4c25759369d3826ef9a8fae7932
GRADO EN INGENIERIA EN TECNOLOGIAS DE TELECOMUNICACION <0451550@lab.it.uc3m.es>
2024-02-28 18:58:49 +0100

    Update

--- my_route_lookup.c
+++ my_route_lookup.c
@@ -1,3 +1,23 @@
 #include "io.h"
 #include "my_route_lookup.h"
 #include "utils.h"
+
+typedef struct Node {
+	int *outInterface;
+	struct Node *leftSon;
+	struct Node *rightSon;
+}
+
+void generateTree(uint32_t *prefix, int *prefixLength, int *outInterface, Node *root) {
+	if (root == NULL) return;
+}
+
+int main(int argc, char *argv[]) {
+	if(argc != 3) return -1;
+	if((int result = initializeIO(argv[1], argv[2])) != OK) {
+		printIOExplanationError(result);
+		return -1;
+	}
+	struct node *root = (Node *) malloc(sizeof(Node));
+	freeIO();
+}
